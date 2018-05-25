8ead3ea3cedfacaa0e4b640a163a0284189f6e57
Baldrani <mael.mayon@free.fr>
2018-05-25 12:42:57 +0200

    Add Console.log exemple on provider

--- src/providers/space-xapi/space-xapi.ts
+++ src/providers/space-xapi/space-xapi.ts
@@ -3,17 +3,17 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map';
 import { IRockets } from '../../services/IRockets';
+import {subscribeToResult} from "rxjs/util/subscribeToResult";
 
 @Injectable()
 export class SpaceXapiProvider {
 
   apiRoot:string = "https://api.spacexdata.com/v2";
-
   constructor(private http: HttpClient) {}
 
   getLaunches(name = null) : Observable<IRockets[]> {
       let apiURL = `${this.apiRoot}/rockets`;
-      console.log(this.http.get(apiURL).map((res: Response) => res.json()));
+      console.log(this.http.get<IRockets[]>(apiURL).subscribe((res) => { console.log(res) }));
       return this.http.get<IRockets[]>(apiURL);
   }
 }
