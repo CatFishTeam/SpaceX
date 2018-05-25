24e6775f546dd1d3a0b340071fc971565ad48083
Baldrani <mael.mayon@free.fr>
2018-05-25 12:39:11 +0200

    Add Start of Rockets Interface

--- src/providers/space-xapi/space-xapi.ts
+++ src/providers/space-xapi/space-xapi.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+import { IRockets } from '../../services/IRockets';
 
 @Injectable()
 export class SpaceXapiProvider {
@@ -9,9 +11,9 @@ export class SpaceXapiProvider {
 
   constructor(private http: HttpClient) {}
 
-  getLaunches(name = null) : Observable<any> {
+  getLaunches(name = null) : Observable<IRockets[]> {
       let apiURL = `${this.apiRoot}/rockets`;
-      console.log(this.http.get(apiURL))
-      return this.http.get(apiURL);
+      console.log(this.http.get(apiURL).map((res: Response) => res.json()));
+      return this.http.get<IRockets[]>(apiURL);
   }
 }
