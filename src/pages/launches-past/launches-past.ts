60f1e15426466d2611dcf2c15ed367de2ceb7ff3
Robilol <pimous78920@gmail.com>
2018-06-28 19:29:23 +0200

    add date filter

--- src/pages/launches-past/launches-past.ts
+++ src/pages/launches-past/launches-past.ts
@@ -17,17 +17,52 @@ import { SpaceXapiProvider } from "../../providers/space-xapi/space-xapi";
 export class LaunchesPastPage {
 
   launches : any;
+  dates : any = { lower: 2000, upper: 2020 };
+  searchQuery: string = '';
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public api: SpaceXapiProvider) {
+    this.initializeLaunches();
   }
 
-  ionViewDidLoad(){
+  initializeLaunches() {
     this.api.getPreviousLaunches().subscribe( data => this.launches = data);
   }
 
   swipe(event) {
     if (event.direction === 2) {
       this.navCtrl.parent.select(1);
+    }
+  }
+
+  launchesDateFilter(ev: any) {
+    // Reset items back to all of the items
+
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.launches = this.launches.filter((item) => {
+        return (item.mission_name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
+  launchesNameFilter(ev: any) {
+    // Reset items back to all of the items
+
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+
+    if (val == "" || typeof val == 'undefined') {
+      this.initializeLaunches();
+    }
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() !== '') {
+      this.launches = this.launches.filter((item) => {
+        return (item.mission_name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
     }
   }
 
