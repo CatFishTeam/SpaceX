41eef68ba097539f9a890ccdefbfb29b68a90487
Robilol <pimous78920@gmail.com>
2018-06-28 22:17:14 +0200

    add redirection on rocket and launchpad

--- src/pages/detail-launch/detail-launch.ts
+++ src/pages/detail-launch/detail-launch.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {SpaceXapiProvider} from "../../providers/space-xapi/space-xapi";
+import {DetailRocketPage} from "../detail-rocket/detail-rocket";
+import {DetailLaunchpadPage} from "../detail-launchpad/detail-launchpad";
 
 /**
  * Generated class for the DetailLaunchPage page.
@@ -20,6 +22,17 @@ export class DetailLaunchPage {
   launch : any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public api: SpaceXapiProvider) {
+  }
+
+  rocketDetail(rocketId) {
+    this.navCtrl.push(DetailRocketPage, { id: rocketId });
+  }
+
+  openModal(launchpadId) {
+
+    this.navCtrl.push(DetailLaunchpadPage, { id: launchpadId });
+    //let modal = this.modalCtrl.create(DetailLaunchpadPage, charNum);
+    //modal.present();
   }
 
   ionViewDidLoad() {
--- src/pages/launches-futur/launches-futur.ts
+++ src/pages/launches-futur/launches-futur.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { SpaceXapiProvider } from "../../providers/space-xapi/space-xapi";
+import {DetailLaunchPage} from "../detail-launch/detail-launch";
 
 /**
  * Generated class for the LaunchesFuturPage page.
@@ -17,12 +18,14 @@ import { SpaceXapiProvider } from "../../providers/space-xapi/space-xapi";
 export class LaunchesFuturPage {
 
   launches : any;
+  dates : any = { lower: 2018, upper: 2030 };
+  searchQuery: string = '';
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public api: SpaceXapiProvider) {
-
+    this.initializeLaunches();
   }
 
-  ionViewDidLoad(){
+  initializeLaunches() {
     this.api.getUpcomingLaunches().subscribe( data => this.launches = data);
   }
 
@@ -31,6 +34,43 @@ export class LaunchesFuturPage {
     if(event.direction === 4) {
       this.navCtrl.parent.select(0);
     }
+  }
+
+  launchesDateFilter(ev: any) {
+    // Reset items back to all of the items
+
+
+    // set val to the value of the searchbar
+    const val = ev.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.lower != '' && val.upper != '') {
+      this.launches = this.launches.filter((item) => {
+        return (item.launch_year >= val.lower && item.launch_year <= val.upper);
+      })
+    }
+  }
+
+  launchesNameFilter(ev: any) {
+    // Reset items back to all of the items
+
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+
+    if (val == "" || typeof val == 'undefined') {
+      this.initializeLaunches();
+    }
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() !== '') {
+      this.launches = this.launches.filter((item) => {
+        return (item.mission_name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
+  launchDetail(flightNumber) {
+    this.navCtrl.push(DetailLaunchPage, { id: flightNumber });
   }
 
 }
