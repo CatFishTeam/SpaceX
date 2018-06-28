c7d9e3e0f8d889b6724356fa7a77f0d32a2a4415
Baldrani <mael.mayon@free.fr>
2018-06-28 12:49:52 +0200

    Add a few things :rocket:

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,21 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { SpaceXapiProvider } from '../../providers/space-xapi/space-xapi';
+import {Observable} from "rxjs/Observable";
+import 'rxjs/add/observable/interval'
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/take'
 
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
-  rockets:any;
+  launch:any;
+  date:any;
+  counter:any;
   constructor(
     public navCtrl: NavController,
     public api: SpaceXapiProvider
   ){}
 
   ionViewDidLoad(){
-    this.api.getRockets().subscribe(data => this.rockets = data);
+    this.api.getNextLaunche().subscribe( data => {this.launch = data },
+    error => console.log("Error: ", error),
+    () => {
+      this.date = this.launch.launch_date_unix - Math.round(Date.now() / 1000);
+      Observable.interval(1000).map((x) => { --this.date });
+      console.log(this.date);
+    })
   }
-
 }
